63a852d466680dc6b7c00eb2a7549b674d28304f
ayushanand18 <kingwarrior880@gmail.com>
2022-07-20 18:01:50 +0530

    Fix. Resolved #32
    Desc. With this commit I have resolved #32 and added a null-check function to ensure species with no occurrence records throw no error when accessing MoF

--- pyobis/occurrences/occurrences.py
+++ pyobis/occurrences/occurrences.py
@@ -80,7 +80,7 @@ def search(scientificname=None,taxonid=None,nodeid=None,datasetid=None,startdate
     res=obis_GET(url, args, 'application/json; charset=utf-8', **kwargs)
     out["results"]+=res["results"]
     
-    if mof:
+    if mof and out["total"]>0:
         mofNormalized = pd.json_normalize(out["results"], "mof", ["id"])
         merged = pd.merge(pd.DataFrame(out["results"]),mofNormalized,on='id',how='inner')
         return merged
