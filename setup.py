6cd28fb50fd3c31453ec37d84caf809ed87684c2
ayushanand18 <kingwarrior880@gmail.com>
2022-08-05 20:46:23 +0530

    Update. Added setupy.py, setup.cfg

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    # The package metadata is specified in setup.cfg but GitHub's downstream dependency graph
+    # does not work unless we put the name this here too.
+    name="pyobis",
+    use_scm_version={
+        "write_to": "pyobis/_version.py",
+        "write_to_template": '__version__ = "{version}"',
+        "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
+    },
+)
