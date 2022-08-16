89ac228a8999a13131d16bf0607e534acf227cda
ayushanand18 <kingwarrior880@gmail.com>
2022-08-16 20:29:55 +0530

    fixing module import error in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,12 +12,11 @@
 import os
 import sys
 
-import pyobis
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(".."))
+import pyobis
 
 # -- General configuration ------------------------------------------------
 
