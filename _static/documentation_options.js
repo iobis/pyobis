9eed73df33b389dfc98b02558c8cf1e2594af232
7yl4r <7yl4r@users.noreply.github.com>
2022-09-15 18:40:08 +0000

    deploy: 99a589d4ffec2d340d654d1c72b6ac24009cf9cd

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.7.dev6+g1a96982',
+    VERSION: '1.2.7.dev18+g99a589d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
