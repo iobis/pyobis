5a20efe2eb587ddbd06e5d6a33fb5f67cc2e8301
ayushanand18 <kingwarrior880@gmail.com>
2022-11-21 19:37:06 +0530

    update. updated request headers to increase speed

--- pyobis/obisutils.py
+++ pyobis/obisutils.py
@@ -28,9 +28,11 @@ def obis_GET(url, args, ctype, **kwargs):
     Handles technical details of sending GET request to the API
     """
     headers = {
-        "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko)\
-         Chrome/81.0.4044.141 Safari/537.36",
-        "Connection": "close",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)\
+         Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Host": "api.obis.org",
+        "Connection": "keep-alive",
     }
     out = requests.get(url, params=args, headers=headers, **kwargs)
     out.raise_for_status()
