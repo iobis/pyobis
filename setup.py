6d456a21ff569af2d235ba9e5eb055533518b1b1
ayushanand18 <kingwarrior880@gmail.com>
2022-11-25 19:12:12 +0530

    fix. temporary fix

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
     # The package metadata is specified in setup.cfg but GitHub's downstream dependency graph
     # does not work unless we put the name this here too.
     name="pyobis",
-    use_scm_version={
-        "write_to": "pyobis/_version.py",
-        "write_to_template": '__version__ = "{version}"',
-        "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
-    },
+    version = "1.3.0",
+    # use_scm_version={
+    #     "write_to": "pyobis/_version.py",
+    #     "write_to_template": '__version__ = "1.3.0"',
+    #     "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
+    # },
 )
