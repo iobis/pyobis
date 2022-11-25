a54fd92e3e464250b07bf6baa314c76c19e4a759
ayushanand18 <kingwarrior880@gmail.com>
2022-11-25 19:21:31 +0530

    fix. fixed pre-commit failings

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     # The package metadata is specified in setup.cfg but GitHub's downstream dependency graph
     # does not work unless we put the name this here too.
     name="pyobis",
-    version = "1.3.0",
+    version="1.3.0",
     # use_scm_version={
     #     "write_to": "pyobis/_version.py",
     #     "write_to_template": '__version__ = "1.3.0"',
