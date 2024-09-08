24c6248001b453e691e1bbe82032e9f7e840fce5
Ayush Anand <36472216+ayushanand18@users.noreply.github.com>
2024-09-08 18:56:30 +0000

    feat: changes print -> logger.info

--- pyobis/occurrences/occurrences.py
+++ pyobis/occurrences/occurrences.py
@@ -16,6 +16,7 @@ from ..obisutils import (
     handle_arrint,
     handle_arrstr,
     obis_baseurl,
+    logger,
     obis_GET,
 )
 
@@ -71,10 +72,10 @@ class OccResponse:
                 # the total time can be estimated easily although this might not be accurate
                 # for larger than 100k records, because the network download takes
                 # even smaller fraction of the total round-trip time
-                print(
-                    f"{self.__total_records} to be fetched. Estimated time = ",
-                    (ending_time - starting_time) * 0.995,
-                    f" {(ending_time - starting_time) * 0.005 * self.__total_records:.0f} ",
+                logger.info(
+                    f"{self.__total_records} to be fetched. Estimated time ="
+                    f"{(ending_time - starting_time) * 0.995}"
+                    f"{(ending_time - starting_time) * 0.005 * self.__total_records:.0f} "
                     "seconds",
                 )
 
@@ -115,17 +116,11 @@ class OccResponse:
                 if "id" not in outdf.columns:
                     break
                 self.__args["size"] = 10000
-                print(
-                    "{}[{}{}] {}/{}".format(
-                        "Fetching: ",
-                        "█" * int((i - 1) * 100 / size),
-                        "." * (100 - int((i + 1) * 100 / size)),
-                        i,
-                        size,
-                    ),
-                    end="\r",
-                    file=sys.stdout,
-                    flush=True,
+                logger.info(
+                    f"Fetching: "
+                    f"[{"█" * int((i - 1) * 100 / size)}"
+                    f"{"." * (100 - int((i + 1) * 100 / size))}]"
+                    f" {i}/{size}"
                 )
                 res = obis_GET(
                     self.__url,
@@ -146,11 +141,8 @@ class OccResponse:
             self.__args["size"] = size % 10000
             # we have already fetched records as a set of 5000 records each time,
             # now we need to get remaining records from the total
-            print(
+            logger.info(
                 "{}[{}{}] {}/{}".format("Fetching: ", "█" * 100, "." * 0, size, size),
-                end="\r",
-                file=sys.stdout,
-                flush=True,
             )
             res = obis_GET(
                 self.__url,
@@ -162,7 +154,7 @@ class OccResponse:
                 [outdf.infer_objects(), pd.DataFrame(res["results"]).infer_objects()],
                 ignore_index=True,
             )
-            print(f"\nFetched {size} records.")
+            logger.info(f"\nFetched {size} records.")
 
             if mof and self.__total_records > 0:
                 mofNormalized = pd.json_normalize(
